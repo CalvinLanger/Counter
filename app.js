242bf7bda66a2788d25df729608240c06d363b52
Calvin Langer <calvin.langerr@gmail.com>
2023-03-11 17:23:40 +0100

    fix responsive

--- app.js
+++ app.js
@@ -3,28 +3,42 @@ let count = 0;
 const value = document.querySelector('#value');
 const buttons = document.querySelectorAll('.btn');
 
-buttons.forEach(function(item) {
-    item.addEventListener('click', function(event) {
+buttons.forEach(function (item) {
+    item.addEventListener('click', function (event) {
         const styles = event.currentTarget.classList;
         if (styles.contains('decrease')) {
             count--;
-        } else if(styles.contains('increase')) {
+        } else if (styles.contains('increase')) {
             count++;
         } else {
             count = 0;
         }
         value.textContent = count;
         valueColorStyle();
-        
+
     });
 });
 
 function valueColorStyle() {
     if (count > 0) {
         value.style.color = 'green';
-    } else if(count < 0) {
+    } else if (count < 0) {
         value.style.color = 'red';
     } else {
         value.style.color = 'black';
     };
 };
+
+const btn = document.querySelectorAll(".btn");
+
+for (i = 0; i < btn.length; i++) {
+
+    btn[i].addEventListener('click', (event) => {
+
+        event.target.style.backgroundColor = "#FFF";
+        setTimeout(() => {
+            event.target.style.backgroundColor = "transparent";
+        }, 500);
+    });
+
+}
